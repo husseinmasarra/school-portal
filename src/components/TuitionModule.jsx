d78b12606fcca16c7e56b629e4956b1b9180292a
husseinmasarra <husseinmassara@gmail.com>
2026-08-25 23:40:55 +0300

    fix: reset top offset to 0 to show complete receipt header on print

--- src/components/TuitionModule.jsx
+++ src/components/TuitionModule.jsx
@@ -889,7 +889,7 @@ export const TuitionModule = () => {
 
                 .receipt-printable-card, html.dark .receipt-printable-card {
                   position: absolute !important;
-                  top: -35mm !important;
+                  top: 0 !important;
                   left: 50% !important;
                   transform: translateX(-50%) !important;
                   margin: 0 !important;

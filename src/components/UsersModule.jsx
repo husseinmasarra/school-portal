7de7dec1b08a526cef36a99fc55ee4a4020452f7
husseinmasarra <husseinmassara@gmail.com>
2026-08-28 09:45:31 +0300

    fix: import missing useEffect in UsersModule to fix ReferenceError

--- src/components/UsersModule.jsx
+++ src/components/UsersModule.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { useApp, defaultAvatars, systemPermissionOptions } from '../context/AppContext';
 import { 
